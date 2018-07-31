d4556fded66b0822eca1f372b8557c61a722928b
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-08-01 00:25:18 +0530

    Add helper function to draw elu skeleton

--- elu-ani-importer/EluLibrary/blenderfunctions.py
+++ elu-ani-importer/EluLibrary/blenderfunctions.py
@@ -51,3 +51,56 @@ def clear_blender():
 def reload_blender():
     bpy.ops.wm.open_mainfile(filepath=bpy.data.filepath)
 
+
+def draw_elu_skeleton(elu_mesh_obj):
+    armature = bpy.data.armatures.new("root_armature")
+    armature_obj = bpy.data.objects.new("armature_obj", armature)
+
+    current_scene = bpy.context.scene
+    current_scene.objects.link(armature_obj)
+    current_scene.objects.active = armature_obj
+
+    armature_obj.select = True
+    armature.draw_type = "STICK"
+    armature_obj.show_x_ray = True
+    armature.show_names = True
+
+    current_scene.update()
+
+    if bpy.ops.object.mode_set.poll():
+        bpy.ops.object.mode_set(mode="EDIT")
+    else:
+        print("mode_set() context is incorrect. Current mode is: {0}".format(bpy.context.mode))
+        return
+
+    for EluMeshNode in elu_mesh_obj.EluMeshNodes:
+        EluMeshNode.BlenderLocalMatrix = Matrix(EluMeshNode.LocalMatrix.GetMatrixAsTuples())
+        EluMeshNode.BlenderLocalMatrix.transpose()
+
+        # Default BlenderGlobalMatrix is same BlenderLocalMatrix
+        EluMeshNode.BlenderGlobalMatrix = EluMeshNode.BlenderLocalMatrix
+
+    bone_names_list = []
+    for EluMeshNode in elu_mesh_obj.EluMeshNodes:
+        bone_name = EluMeshNode.NodeName
+        bone_names_list.append(bone_name)
+        edit_bone = armature.edit_bones.new(bone_name)
+        edit_bone.tail = edit_bone.head + Vector((0.0, 0.0, 0.5))
+        parent_name = EluMeshNode.NodeParentName
+        parent_id = EluMeshNode.ParentNodeID
+        if parent_id >= 0 and parent_name != globalvars.STRING_NONE:
+            parent_edit_bone = armature.edit_bones[parent_name]
+            edit_bone.parent = parent_edit_bone
+
+    for edit_bone in armature.edit_bones:
+        if edit_bone.parent is None:
+            for EluMeshNode in elu_mesh_obj.EluMeshNodes:
+                if edit_bone.name == EluMeshNode.NodeName:
+                    edit_bone.matrix = EluMeshNode.BlenderLocalMatrix
+        else:
+            for EluMeshNode in elu_mesh_obj.EluMeshNodes:
+                if edit_bone.name == EluMeshNode.NodeName:
+                    EluMeshNode.BlenderGlobalMatrix = edit_bone.parent.matrix * EluMeshNode.BlenderLocalMatrix
+                    edit_bone.matrix = EluMeshNode.BlenderGlobalMatrix
+
+    bpy.ops.object.mode_set(mode="OBJECT")
