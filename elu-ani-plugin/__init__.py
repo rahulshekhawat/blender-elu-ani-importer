22e2f037d8cd3d1fd74ac60bf199c795e854410a
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-07-31 16:46:14 +0530

    Add __init__.py (bl_info)

---
+++ elu-ani-plugin/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# pylint: disable=C0111
+# pylint: disable=C0103
+# pylint: disable=W0703
+
+bl_info = {
+    "name" : "elu/ani format",
+    "author" : "Rahul Shekhawat",
+    "version" : (1, 0, 0),
+    "blender" : (2, 79, 0),
+    "location" : "File > Import-Export",
+    "description" : "This plugin can be used to import elu or ani files inside blender",
+    "category" : "Import-Export"
+}
+
+# class ImportElu(bpy.types.Operator, ImportHelper):
+#     """Load an elu file"""
+
+def register():
+    pass
+
+def unregister():
+    pass
