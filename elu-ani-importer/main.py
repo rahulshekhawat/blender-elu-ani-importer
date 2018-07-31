82f1296e666ec27af6abaf76a7765ea13d42e819
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-08-01 00:20:24 +0530

    Add main.py and blend files

---
+++ elu-ani-importer/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# pylint: disable=C0111
+# pylint: disable=C0103
+# pylint: disable=W0703
+
+"""
+* elu files loader and exporter for blender
+* Run this script from inside blender.
+"""
+
+__author__ = "Ryujin Ishima"
+__copyright__ = ""
+__credits__ = "[Ryujin Ishima]"
+__license__ = ""
+__version__ = "1.0.0"
+__maintainer__ = "Ryujin Ishima"
+__email__ = "ryujin.hawk@gmail.com"
+__status__ = "Production"
+
+
